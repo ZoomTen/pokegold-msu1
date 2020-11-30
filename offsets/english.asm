9012515d46eeed996565081030bcedbd5b110cfd
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2020-11-30 11:06:36 +0700

    disable automatic music fading on sgb (music already playing)

--- offsets/english.asm
+++ offsets/english.asm
@@ -16,11 +16,8 @@ ENDC
 ; stuff to patch
 ; home bank
 	define_home DelayFrame,       $32e
-	; E5 D5 C5 F5 F0 9F F5 3E 3A E0 9F EA 00 20 7B A7 28 05 CD ?? ?? 18 03 CD 00 40 F1 E0 9F EA 00 20 F1 C1 D1 E1 C9
 	define_home PlayMusic,       $3d98
-	; E5 D5 C5 F5 F0 9F F5 3E 3A E0 9F EA 00 20 D5 11 00 00 CD ?? ?? CD ?? ?? D1 CD ?? ?? F1 E0 9F EA 00 20 F1 C1 D1 E1 C9
 	define_home PlayMusic2,      $3dbd
-	; E5 D5 C5 F5 CD ?? ?? FA ?? ?? BB 28 11 3E 08 EA ?? ?? 7B EA ?? ?? 7A EA ?? ?? 7B EA ?? ?? F1 C1 D1 E1 C9
 	define_home FadeToMapMusic,  $3e9a
 
 ; bank 02
@@ -40,6 +37,9 @@ ENDC
 
 ; bank 3a
 	define_location _InitSound,      $e8000
-	; CD ?? ?? 21 ?? ?? 73 23 72 21 ?? ?? 19 19 19 2A ...
 	define_location _PlayMusic,      $e8b30
 	define_location FadeMusic,  $3a, $4358
+; under FadeMusic.novolume
+	define_location FadeMusic_LoadNewSong1, $3a, $438f
+; under FadeMusic.bicycle
+	define_location FadeMusic_LoadNewSong2, $3a, $43ab
--- patches/fade_music.asm
+++ patches/fade_music.asm
@@ -1,12 +1,28 @@
 SECTION "Fade Music", ROMX[FadeMusic], BANK[BANK_FadeMusic]
 PATCH_FadeMusic::
-	jp PATCH_FadeMusic_CheckFade
+	jp FadeMusic_CheckFade
 	nop
 	nop
 PATCH_FadeMusic_Continue:
 
-SECTION "Fade Music (Home)", ROMX[Bank3a_FreeSpace], BANK[$3a]
-PATCH_FadeMusic_CheckFade::
+SECTION "Load Audio from Fade 1", ROMX[FadeMusic_LoadNewSong1], BANK[BANK_FadeMusic_LoadNewSong1]
+PATCH_FadeMusic_LoadNewSong1::
+	call FadeMusic_LoadMusicOnGBC
+	pop bc
+	xor a
+	ld [wMusicFade], a
+	ret
+
+SECTION "Load Audio from Fade 2", ROMX[FadeMusic_LoadNewSong2], BANK[BANK_FadeMusic_LoadNewSong2]
+PATCH_FadeMusic_LoadNewSong2::
+	call FadeMusic_LoadMusicOnGBC
+	pop bc
+	ld hl, wMusicFade
+	set 7, [hl]	; set MUSIC_FADE_IN_F, [hl]
+	ret
+
+SECTION "Fade Music Redirect", ROMX[Bank3a_FreeSpace], BANK[$3a]
+FadeMusic_CheckFade::
 	ld a, [wMusicFade]
 	and a
 	jr z, .nofade
@@ -20,3 +36,13 @@ PATCH_FadeMusic_CheckFade::
 	ld [wIsInFade], a
 	ld [wCheckAndFadeMusicID], a
 	ret
+
+FadeMusic_LoadMusicOnGBC:
+	ld a, [hSGB]
+	and a
+	ret nz
+	ld a, [wMusicFadeID]
+	ld e, a
+	ld a, [wMusicFadeID+1]
+	ld d, a
+	jp _PlayMusic
